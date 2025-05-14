ac19e323ca05a61990d7005ec60f36787d48734a
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-05-14 23:58:34 +0530

    implement protected route feature from client side with context api

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import Login from "@/components/Login";
+import { useAuth } from "@/context/authContext";
+
+const LoginPage = () => {
+    const router = useRouter();
+    const { authStatus } = useAuth();
+
+    
+    if (authStatus) {
+        router.replace("/profile");
+        return <></>;
+    }
+
+    return(
+        <section className="px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+            <Login />
+        </section>
+    )
+}
+
+
+export default LoginPage;
